aac4024562b9f54e36577e9348bc4650c380985f
Robert Leduc <rleduc34@gmail.com>
2020-05-30 02:13:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,25 +1,30 @@
 class School
-  attr_accessor :name, :roster
-
-  def initialize (name)
-  @name = name
-  @roster = {}
+  class School
+  def initialize(name)
+    @name = name
+    @roster = {}
   end
-
-  def add_student (name, grade)
-  roster[grade] ||= []
-  roster[grade] << name
+  def roster
+    @roster
   end
-
-    def grade(grade)
-      @roster[grade]
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
     end
-
-
+  end
+  def grade(grade)
+    @roster[grade]
+  end
   def sort
-  @roster.each do |key, value|
+    @roster.each do |key, value|
       value.sort!
     end
+  end
 end
-end
-end
+
+
+
+
+
