431512eb7862add22bfbb941336ae813eaa536f2
Robert Leduc <rleduc34@gmail.com>
2020-05-30 01:35:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,16 +4,18 @@ class School
   def initialize (name)
   @name = name
   @roster = {}
-end
-def add_student (student, level)
+  end
+
+  def add_student (student, level)
   roster[level] ||= []
   roster[level] << student
-end
-def grade (level)
+  end
+
+  def grade (level)
   roster.detect do |x, y|
     if x == level
       return y
     end
   end
-
+end
 end
