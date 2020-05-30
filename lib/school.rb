aabd542b03a5558a6fbceac481af4cfeb83a5e7c
Robert Leduc <rleduc34@gmail.com>
2020-05-30 01:35:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,11 @@ def add_student (student, level)
   roster[level] ||= []
   roster[level] << student
 end
-
-
+def grade (level)
+  roster.detect do |x, y|
+    if x == level
+      return y
+    end
+  end
 
 end
